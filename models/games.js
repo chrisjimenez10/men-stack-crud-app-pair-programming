56401eb4459b759d5ae6d84ffa3d9d1d7ca3f678
chris <jimenezchristopher5@gmail.com>
2024-04-30 09:22:57 -0500

    completed all required files and npm packages and 4 routes

---
+++ models/games.js
@@ -0,0 +1,40 @@
+//Import
+const mongoose = require("mongoose");
+
+//Schema
+const VideoGameSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    platform: {
+        type: String,
+        required: true,
+    },
+    genre: {
+        type: String,
+        required: true,
+    },
+    release: {
+        type: Number,
+        required: true,
+    },
+    publisher: {
+        type: String,
+        required: true,
+    },
+    rating: {
+        type: Number,
+        required: true,
+    },
+    status: {
+        type: Boolean,
+        required: true,
+    },
+});
+
+//Model
+const VideoGame = mongoose.model("Video_Game", VideoGameSchema);
+
+//Export Model
+module.exports = VideoGame;
---
+++ server.js
@@ -0,0 +1,62 @@
+//Import
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const VideoGame = require("./models/games.js");
+
+//Connect Database
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on("connected", ()=>{
+    console.log(`connected to MongoDB ${mongoose.connection.name}`);
+})
+
+//Middelware
+app.use(methodOverride("_method"));
+app.use(morgan("dev"));
+// app.use(express.static("public"));
+app.use(express.urlencoded({extended:false}));
+
+//Start Server
+const port = 4000;
+app.listen(port, ()=>{
+    console.log(`Listening on Port ${port}`);
+})
+
+//Routes
+app.get("/", (req, res)=>{
+    res.render("home.ejs")
+})
+    //Index Show Page
+app.get("/index", async (req, res)=>{
+    const videoGameList = await VideoGame.find();
+    res.render("index.ejs", {
+        videoGameList
+    })
+})
+    //Add Video Game Show Page
+app.get("/index/new", (req, res)=>{
+    res.render("new.ejs");
+})
+
+app.post("/index/new", async (req, res)=>{
+    if(req.body.status){
+        req.body.status = true;
+    }
+    await VideoGame.create(req.body);
+    res.redirect("/index");
+})
+    //Video Game Details Show Page
+app.get("/index/:id", async (req, res)=>{
+    const foundGame = await VideoGame.findById(req.params.id);
+    res.render("show.ejs", {
+        foundGame
+    })
+})
+    //Delete Video Game
+app.delete("/index/:id", async (req, res)=>{
+    await VideoGame.findByIdAndDelete(req.params.id);
+    res.redirect("/index");
+})
